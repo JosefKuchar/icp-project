a8ffb23a8fcf0b9d7dbbcd4eab4cbd5f8de54645
Josef Kuchař <josef.kuchar267@gmail.com>
2023-02-28 19:35:27 +0100

    Proper asset loading

--- src/ICP.pro
+++ src/ICP.pro
@@ -18,6 +18,8 @@ HEADERS += \
 FORMS += \
     mainwindow.ui
 
+RESOURCES = assets.qrc
+
 # Default rules for deployment.
 qnx: target.path = /tmp/$${TARGET}/bin
 else: unix:!android: target.path = /opt/$${TARGET}/bin
--- src/main.cpp
+++ src/main.cpp
@@ -16,12 +16,19 @@ class Grid : public QGraphicsScene {
         for (int row = 0; row < rows; ++row) {
             for (int col = 0; col < cols; ++col) {
                 Sprite* sprite = new Sprite(m_spritePixmap);
-                std::cout << m_spritePixmap.width() << std::endl;
-                ;
                 sprite->setPos(col * (m_spritePixmap.width() + 5),
                                row * (m_spritePixmap.height() + 5));
                 addItem(sprite);
+                m_sprites.append(sprite);
             }
+        }
+    }
+
+    void updatePositions() {
+        for (auto sprite : m_sprites) {
+            QPointF pos = sprite->pos();
+            pos.setX(pos.x() + 1.0f);  // Update the x position of the sprite
+            sprite->setPos(pos);
         }
     }
 
@@ -29,21 +36,25 @@ class Grid : public QGraphicsScene {
     int m_rows;
     int m_cols;
     QPixmap m_spritePixmap;
+    QList<Sprite*> m_sprites;
 };
 
 int main(int argc, char* argv[]) {
     QApplication a(argc, argv);
 
-    QPixmap spritePixmap("sprite.png");
-    if (spritePixmap.isNull())
-        std::cout << "darn" << std::endl;
-    else
-        std::cout << "not null" << std::endl;
+    QPixmap spritePixmap(":sprite.png");
     Grid grid(10, 10, spritePixmap);
     QGraphicsView view(&grid);
     view.show();
 
-    MainWindow w;
-    w.show();
+    QTimer timer;
+    QObject::connect(&timer, &QTimer::timeout, [&]() {
+        grid.updatePositions();
+        view.update();
+    });
+    timer.start(16);
+
+    // MainWindow w;
+    // w.show();
     return a.exec();
 }
