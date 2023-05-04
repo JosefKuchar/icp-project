052f18860042359a73c1aa8de950a2783764ebe8
Josef Kuchař <josef.kuchar267@gmail.com>
2023-05-04 18:46:24 +0200

    Handle map error kinda

--- src/page/page_play.cpp
+++ src/page/page_play.cpp
@@ -4,14 +4,27 @@
 #include <QVBoxLayout>
 #include "../mainwindow.hpp"
 #include "maploader.hpp"
+#include "page.hpp"
 
 PlayPage::PlayPage(QWidget* parent) : QWidget(parent) {}
 
 void PlayPage::showEvent(QShowEvent* event) {
     MainWindow* window = (MainWindow*)this->parentWidget()->parentWidget();
-    MapInfo map = MapInfo(window->mapPath);
-    m_gameScene = new GameScene(map);
-    m_gameScene->start();
+    try {
+        MapInfo map = MapInfo(window->mapPath);
+        m_gameScene = new GameScene(map);
+        m_gameScene->start();
+    } catch (std::exception& e) {
+        // Show error dialog
+        QMessageBox msgBox;
+        msgBox.setWindowTitle("Error");
+        msgBox.setText("Error loading map");
+        msgBox.exec();
+        // Go back to menu
+        QStackedWidget* widget = (QStackedWidget*)this->parentWidget();
+        widget->setCurrentIndex((int)Page::Menu);
+        return;
+    }
 
     QGraphicsView* view = new QGraphicsView(m_gameScene);
 
